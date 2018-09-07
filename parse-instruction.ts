3ce0c397cdeafabb58feaa32b6aeeea8495e9fc4
Caleb Sander <csander@purestorage.com>
2018-09-07 10:35:52 -0700

    Add parsers for remaining instructions

--- parse-instruction.ts
+++ parse-instruction.ts
@@ -182,15 +182,15 @@ type NumericInstruction
 
 			'i32.wrap' |
 			'i32.trunc_s/f32' |
-			'i32.truns_u/f32' |
+			'i32.trunc_u/f32' |
 			'i32.trunc_s/f64' |
-			'i32.truns_u/f64' |
+			'i32.trunc_u/f64' |
 			'i64.extend_s' |
 			'i64.extend_u' |
 			'i64.trunc_s/f32' |
-			'i64.truns_u/f32' |
+			'i64.trunc_u/f32' |
 			'i64.trunc_s/f64' |
-			'i64.truns_u/f64' |
+			'i64.trunc_u/f64' |
 			'f32.convert_s/i32' |
 			'f32.convert_u/i32' |
 			'f32.convert_s/i64' |
@@ -213,7 +213,6 @@ type Instruction
 	| MemoryInstruction
 	| NumericInstruction
 
-const instructionParsers = new Map<number, Parser<Instruction>>()
 export const parseInstruction = parseAndThen(
 	parseByte,
 	opcode => {
@@ -289,20 +288,16 @@ const parseUnsigned: Parser<number> = parseAndThen(
 				)
 			: parseReturn(n)
 )
-// TODO: verify that this computation is correct
-const parseSigned: Parser<bigint> = data => {
-	let value = 0n
-	let shift = 0
-	let length = 0
-	let byte: number;
-	do {
-		byte = data.getUint8(length++)
-		value |= BigInt(byte & 0b01111111) << BigInt(shift)
-		shift += 7
-	} while (byte & 0b10000000)
-	if (byte & 0b01000000) value |= -1n << BigInt(shift)
-	return {value, length}
-}
+const parseSigned: Parser<bigint> = parseAndThen(
+	parseByte,
+	n =>
+		n & 0b10000000
+			? parseMap(
+					parseSigned,
+					m => m << 7n | BigInt(n & 0b01111111)
+				)
+			: parseReturn(BigInt(n << 25 >> 25)) // sign-extend bit 6
+)
 const parseVector = <A>(parser: Parser<A>) => parseAndThen(
 	parseUnsigned,
 	parseTimes(parser)
@@ -321,114 +316,232 @@ const parseMemoryAccess = parseAndThen(
 const parseMemoryInstruction = <TYPE extends string>(type: TYPE) =>
 	parseMap(parseMemoryAccess, access => ({type, access}))
 
-instructionParsers
-	.set(0x00, parseFixedInstruction('unreachable'))
-	.set(0x01, parseFixedInstruction('nop'))
-	.set(0x02, parseBlockLike('block'))
-	.set(0x03, parseBlockLike('loop'))
-	.set(0x04, parseAndThen(
+const MEMORY_SIZE: Instruction = {type: 'memory.size'}
+const MEMORY_GROW: Instruction = {type: 'memory.grow'}
+
+const instructionParsers = new Map<number, Parser<Instruction>>([
+	[0x00, parseFixedInstruction('unreachable')],
+	[0x01, parseFixedInstruction('nop')],
+	[0x02, parseBlockLike('block')],
+	[0x03, parseBlockLike('loop')],
+	[0x04, parseAndThen(
 		parseResultType,
 		returns => parseMap(
 			parseIfBody,
 			(ifBody): Instruction =>
 				({type: 'if', returns, ...ifBody})
 		)
-	))
-	.set(0x0C, parseMap(
+	)],
+	[0x0C, parseMap(
 		parseUnsigned,
 		(label): Instruction => ({type: 'br', label})
-	))
-	.set(0x0D, parseMap(
+	)],
+	[0x0D, parseMap(
 		parseUnsigned,
 		(label): Instruction => ({type: 'br_if', label})
-	))
-	.set(0x0E, parseAndThen(
+	)],
+	[0x0E, parseAndThen(
 		parseVector(parseUnsigned),
 		cases => parseMap(
 			parseUnsigned,
 			(defaultCase): Instruction =>
 				({type: 'br_table', cases, defaultCase})
 		)
-	))
-	.set(0x0F, parseReturn<Instruction>({type: 'return'}))
-	.set(0x10, parseMap(
+	)],
+	[0x0F, parseReturn<Instruction>({type: 'return'})],
+	[0x10, parseMap(
 		parseUnsigned,
 		(func): Instruction => ({type: 'call', func})
-	))
-	.set(0x11, parseAndThen(
+	)],
+	[0x11, parseAndThen(
 		parseUnsigned,
 		funcType => parseMap(
 			parseExact(0x00),
 			(_): Instruction => ({type: 'call_indirect', funcType})
 		)
-	))
+	)],
 
-	.set(0x1A, parseFixedInstruction('drop'))
-	.set(0x1B, parseFixedInstruction('select'))
+	[0x1A, parseFixedInstruction('drop')],
+	[0x1B, parseFixedInstruction('select')],
 
-	.set(0x20, parseLocalInstruction('get_local'))
-	.set(0x21, parseLocalInstruction('set_local'))
-	.set(0x22, parseLocalInstruction('tee_local'))
-	.set(0x23, parseGlobalInstruction('get_global'))
-	.set(0x24, parseGlobalInstruction('set_global'))
+	[0x20, parseLocalInstruction('get_local')],
+	[0x21, parseLocalInstruction('set_local')],
+	[0x22, parseLocalInstruction('tee_local')],
+	[0x23, parseGlobalInstruction('get_global')],
+	[0x24, parseGlobalInstruction('set_global')],
 
-	.set(0x28, parseMemoryInstruction('i32.load'))
-	.set(0x29, parseMemoryInstruction('i64.load'))
-	.set(0x2A, parseMemoryInstruction('f32.load'))
-	.set(0x2B, parseMemoryInstruction('f64.load'))
-	.set(0x2C, parseMemoryInstruction('i32.load8_s'))
-	.set(0x2D, parseMemoryInstruction('i32.load8_u'))
-	.set(0x2E, parseMemoryInstruction('i32.load16_s'))
-	.set(0x2F, parseMemoryInstruction('i32.load16_u'))
-	.set(0x30, parseMemoryInstruction('i64.load8_s'))
-	.set(0x31, parseMemoryInstruction('i64.load8_u'))
-	.set(0x32, parseMemoryInstruction('i64.load16_s'))
-	.set(0x33, parseMemoryInstruction('i64.load16_u'))
-	.set(0x34, parseMemoryInstruction('i64.load32_s'))
-	.set(0x35, parseMemoryInstruction('i64.load32_u'))
-	.set(0x36, parseMemoryInstruction('i32.store'))
-	.set(0x37, parseMemoryInstruction('i64.store'))
-	.set(0x38, parseMemoryInstruction('f32.store'))
-	.set(0x39, parseMemoryInstruction('f64.store'))
-	.set(0x3A, parseMemoryInstruction('i32.store8'))
-	.set(0x3B, parseMemoryInstruction('i32.store16'))
-	.set(0x3C, parseMemoryInstruction('i64.store8'))
-	.set(0x3D, parseMemoryInstruction('i64.store16'))
-	.set(0x3E, parseMemoryInstruction('i64.store32'))
-	.set(0x3F, parseMap(
-		parseExact(0x00),
-		(_): Instruction => ({type: 'memory.size'})
-	))
-	.set(0x40, parseMap(
-		parseExact(0x00),
-		(_): Instruction => ({type: 'memory.grow'})
-	))
+	[0x28, parseMemoryInstruction('i32.load')],
+	[0x29, parseMemoryInstruction('i64.load')],
+	[0x2A, parseMemoryInstruction('f32.load')],
+	[0x2B, parseMemoryInstruction('f64.load')],
+	[0x2C, parseMemoryInstruction('i32.load8_s')],
+	[0x2D, parseMemoryInstruction('i32.load8_u')],
+	[0x2E, parseMemoryInstruction('i32.load16_s')],
+	[0x2F, parseMemoryInstruction('i32.load16_u')],
+	[0x30, parseMemoryInstruction('i64.load8_s')],
+	[0x31, parseMemoryInstruction('i64.load8_u')],
+	[0x32, parseMemoryInstruction('i64.load16_s')],
+	[0x33, parseMemoryInstruction('i64.load16_u')],
+	[0x34, parseMemoryInstruction('i64.load32_s')],
+	[0x35, parseMemoryInstruction('i64.load32_u')],
+	[0x36, parseMemoryInstruction('i32.store')],
+	[0x37, parseMemoryInstruction('i64.store')],
+	[0x38, parseMemoryInstruction('f32.store')],
+	[0x39, parseMemoryInstruction('f64.store')],
+	[0x3A, parseMemoryInstruction('i32.store8')],
+	[0x3B, parseMemoryInstruction('i32.store16')],
+	[0x3C, parseMemoryInstruction('i64.store8')],
+	[0x3D, parseMemoryInstruction('i64.store16')],
+	[0x3E, parseMemoryInstruction('i64.store32')],
+	[0x3F, parseMap(parseExact(0x00), _ => MEMORY_SIZE)],
+	[0x40, parseMap(parseExact(0x00), _ => MEMORY_GROW)],
 
-	.set(0x41, parseMap(
+	[0x41, parseMap(
 		parseSigned,
 		(n): Instruction => ({type: 'i32.const', value: Number(n)})
-	))
-	.set(0x42, parseMap(
+	)],
+	[0x42, parseMap(
 		parseSigned,
 		(value): Instruction => ({type: 'i64.const', value})
-	))
-	.set(0x43, parseMap(
+	)],
+	[0x43, parseMap(
 		data => ({value: data.getFloat32(0, true), length: 4}),
 		(value): Instruction => ({type: 'f32.const', value})
-	))
-	.set(0x44, parseMap(
+	)],
+	[0x44, parseMap(
 		data => ({value: data.getFloat64(0, true), length: 8}),
 		(value): Instruction => ({type: 'f64.const', value})
-	))
+	)],
 
-	.set(0x45, parseFixedInstruction('i32.eqz'))
-	.set(0x46, parseFixedInstruction('i32.eq'))
-	.set(0x47, parseFixedInstruction('i32.ne'))
-	.set(0x48, parseFixedInstruction('i32.lt_s'))
-	.set(0x49, parseFixedInstruction('i32.lt_u'))
-	.set(0x4A, parseFixedInstruction('i32.gt_s'))
-	.set(0x4B, parseFixedInstruction('i32.gt_u'))
-	.set(0x4C, parseFixedInstruction('i32.le_s'))
-	.set(0x4D, parseFixedInstruction('i32.le_u'))
-	.set(0x4E, parseFixedInstruction('i32.ge_s'))
-	.set(0x4F, parseFixedInstruction('i32.ge_u'))
+	[0x45, parseFixedInstruction('i32.eqz')],
+	[0x46, parseFixedInstruction('i32.eq')],
+	[0x47, parseFixedInstruction('i32.ne')],
+	[0x48, parseFixedInstruction('i32.lt_s')],
+	[0x49, parseFixedInstruction('i32.lt_u')],
+	[0x4A, parseFixedInstruction('i32.gt_s')],
+	[0x4B, parseFixedInstruction('i32.gt_u')],
+	[0x4C, parseFixedInstruction('i32.le_s')],
+	[0x4D, parseFixedInstruction('i32.le_u')],
+	[0x4E, parseFixedInstruction('i32.ge_s')],
+	[0x4F, parseFixedInstruction('i32.ge_u')],
+
+	[0x50, parseFixedInstruction('i64.eqz')],
+	[0x51, parseFixedInstruction('i64.eq')],
+	[0x52, parseFixedInstruction('i64.ne')],
+	[0x53, parseFixedInstruction('i64.lt_s')],
+	[0x54, parseFixedInstruction('i64.lt_u')],
+	[0x55, parseFixedInstruction('i64.gt_s')],
+	[0x56, parseFixedInstruction('i64.gt_u')],
+	[0x57, parseFixedInstruction('i64.le_s')],
+	[0x58, parseFixedInstruction('i64.le_u')],
+	[0x59, parseFixedInstruction('i64.ge_s')],
+	[0x5A, parseFixedInstruction('i64.ge_u')],
+
+	[0x5B, parseFixedInstruction('f32.eq')],
+	[0x5C, parseFixedInstruction('f32.ne')],
+	[0x5D, parseFixedInstruction('f32.lt')],
+	[0x5E, parseFixedInstruction('f32.gt')],
+	[0x5F, parseFixedInstruction('f32.le')],
+	[0x60, parseFixedInstruction('f32.ge')],
+
+	[0x61, parseFixedInstruction('f64.eq')],
+	[0x62, parseFixedInstruction('f64.ne')],
+	[0x63, parseFixedInstruction('f64.lt')],
+	[0x64, parseFixedInstruction('f64.gt')],
+	[0x65, parseFixedInstruction('f64.le')],
+	[0x66, parseFixedInstruction('f64.ge')],
+
+	[0x67, parseFixedInstruction('i32.clz')],
+	[0x68, parseFixedInstruction('i32.ctz')],
+	[0x69, parseFixedInstruction('i32.popcnt')],
+	[0x6A, parseFixedInstruction('i32.add')],
+	[0x6B, parseFixedInstruction('i32.sub')],
+	[0x6C, parseFixedInstruction('i32.mul')],
+	[0x6D, parseFixedInstruction('i32.div_s')],
+	[0x6E, parseFixedInstruction('i32.div_u')],
+	[0x6F, parseFixedInstruction('i32.rem_s')],
+	[0x70, parseFixedInstruction('i32.rem_u')],
+	[0x71, parseFixedInstruction('i32.and')],
+	[0x72, parseFixedInstruction('i32.or')],
+	[0x73, parseFixedInstruction('i32.xor')],
+	[0x74, parseFixedInstruction('i32.shl')],
+	[0x75, parseFixedInstruction('i32.shr_s')],
+	[0x76, parseFixedInstruction('i32.shr_u')],
+	[0x77, parseFixedInstruction('i32.rotl')],
+	[0x78, parseFixedInstruction('i32.rotr')],
+
+	[0x79, parseFixedInstruction('i64.clz')],
+	[0x7A, parseFixedInstruction('i64.ctz')],
+	[0x7B, parseFixedInstruction('i64.popcnt')],
+	[0x7C, parseFixedInstruction('i64.add')],
+	[0x7D, parseFixedInstruction('i64.sub')],
+	[0x7E, parseFixedInstruction('i64.mul')],
+	[0x7F, parseFixedInstruction('i64.div_s')],
+	[0x80, parseFixedInstruction('i64.div_u')],
+	[0x81, parseFixedInstruction('i64.rem_s')],
+	[0x82, parseFixedInstruction('i64.rem_u')],
+	[0x83, parseFixedInstruction('i64.and')],
+	[0x84, parseFixedInstruction('i64.or')],
+	[0x85, parseFixedInstruction('i64.xor')],
+	[0x86, parseFixedInstruction('i64.shl')],
+	[0x87, parseFixedInstruction('i64.shr_s')],
+	[0x88, parseFixedInstruction('i64.shr_u')],
+	[0x89, parseFixedInstruction('i64.rotl')],
+	[0x8A, parseFixedInstruction('i64.rotr')],
+
+	[0x8B, parseFixedInstruction('f32.abs')],
+	[0x8C, parseFixedInstruction('f32.neg')],
+	[0x8D, parseFixedInstruction('f32.ceil')],
+	[0x8E, parseFixedInstruction('f32.floor')],
+	[0x8F, parseFixedInstruction('f32.trunc')],
+	[0x90, parseFixedInstruction('f32.nearest')],
+	[0x91, parseFixedInstruction('f32.sqrt')],
+	[0x92, parseFixedInstruction('f32.add')],
+	[0x93, parseFixedInstruction('f32.sub')],
+	[0x94, parseFixedInstruction('f32.mul')],
+	[0x95, parseFixedInstruction('f32.div')],
+	[0x96, parseFixedInstruction('f32.min')],
+	[0x97, parseFixedInstruction('f32.max')],
+	[0x98, parseFixedInstruction('f32.copysign')],
+
+	[0x99, parseFixedInstruction('f64.abs')],
+	[0x9A, parseFixedInstruction('f64.neg')],
+	[0x9B, parseFixedInstruction('f64.ceil')],
+	[0x9C, parseFixedInstruction('f64.floor')],
+	[0x9D, parseFixedInstruction('f64.trunc')],
+	[0x9E, parseFixedInstruction('f64.nearest')],
+	[0x9F, parseFixedInstruction('f64.sqrt')],
+	[0xA0, parseFixedInstruction('f64.add')],
+	[0xA1, parseFixedInstruction('f64.sub')],
+	[0xA2, parseFixedInstruction('f64.mul')],
+	[0xA3, parseFixedInstruction('f64.div')],
+	[0xA4, parseFixedInstruction('f64.min')],
+	[0xA5, parseFixedInstruction('f64.max')],
+	[0xA6, parseFixedInstruction('f64.copysign')],
+
+	[0xA7, parseFixedInstruction('i32.wrap')],
+	[0xA8, parseFixedInstruction('i32.trunc_s/f32')],
+	[0xA9, parseFixedInstruction('i32.trunc_u/f32')],
+	[0xAA, parseFixedInstruction('i32.trunc_s/f64')],
+	[0xAB, parseFixedInstruction('i32.trunc_u/f64')],
+	[0xAC, parseFixedInstruction('i64.extend_s')],
+	[0xAD, parseFixedInstruction('i64.extend_u')],
+	[0xAE, parseFixedInstruction('i64.trunc_s/f32')],
+	[0xAF, parseFixedInstruction('i64.trunc_u/f32')],
+	[0xB0, parseFixedInstruction('i64.trunc_s/f64')],
+	[0xB1, parseFixedInstruction('i64.trunc_u/f64')],
+	[0xB2, parseFixedInstruction('f32.convert_s/i32')],
+	[0xB3, parseFixedInstruction('f32.convert_u/i32')],
+	[0xB4, parseFixedInstruction('f32.convert_s/i64')],
+	[0xB5, parseFixedInstruction('f32.convert_u/i64')],
+	[0xB6, parseFixedInstruction('f32.demote')],
+	[0xB7, parseFixedInstruction('f64.convert_s/i32')],
+	[0xB8, parseFixedInstruction('f64.convert_u/i32')],
+	[0xB9, parseFixedInstruction('f64.convert_s/i64')],
+	[0xBA, parseFixedInstruction('f64.convert_u/i64')],
+	[0xBB, parseFixedInstruction('f64.promote')],
+	[0xBC, parseFixedInstruction('i32.reinterpret')],
+	[0xBD, parseFixedInstruction('i64.reinterpret')],
+	[0xBE, parseFixedInstruction('f32.reinterpret')],
+	[0xBF, parseFixedInstruction('f64.reinterpret')]
+])
